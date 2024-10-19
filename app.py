9df17a690ac24c23ac517feb544fc6155968b7ea
Patrick Squire <patrickjdsquire@yahoo.com.au>
2024-10-19 11:55:08 +1030

    adding the login route and html

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
-from flask import Flask
+from flask import Flask, request, render_template, redirect, url_for
 
 app = Flask(__name__)
 
+# Function to check credentials
+def check_credentials(username, password):
+    with open('user_data/user_credentials.txt', 'r') as file:
+        for line in file:
+            stored_username, stored_password = line.strip().split(',')
+            if stored_username == username and stored_password == password:
+                return True
+    return False
+
 @app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
+def home():
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+    if check_credentials(username, password):
+        # Save the current user as a variable
+        global current_user
+        current_user = username
+        return f"Welcome, {username}!"
+    else:
+        return "Invalid credentials. Please try again."
+
+if __name__ == '__main__':
+    app.run(debug=True)
