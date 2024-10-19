05d0f8239248fc9387092ce7cb3caac5e1bb2835
Patrick Squire <patrickjdsquire@yahoo.com.au>
2024-10-19 12:07:27 +1030

    added register page and routes

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, render_template, redirect, url_for
 
 app = Flask(__name__)
 
-#change below file for offline debugging
+# Change below file for offline debugging
 user_credentials_file = '/home/PDogg95/MagicApp_4a/user_data/user_credentials.txt'
 
 # Function to check credentials
@@ -14,6 +14,11 @@ def check_credentials(username, password):
                 return True
     return False
 
+# Function to register a new user
+def register_user(username, password):
+    with open(user_credentials_file, 'a') as file:
+        file.write(f"{username},{password}\n")
+
 @app.route('/')
 def home():
     return render_template('login.html')
@@ -23,12 +28,20 @@ def login():
     username = request.form['username']
     password = request.form['password']
     if check_credentials(username, password):
-        # Save the current user as a variable
         global current_user
         current_user = username
         return f"Welcome, {username}!"
     else:
         return "Invalid credentials. Please try again."
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        register_user(username, password)
+        return redirect(url_for('home'))
+    return render_template('register.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
