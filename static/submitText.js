980b38ebd96c29fecb33a2983595cee3280b7f93
Patrick Squire <patrickjdsquire@yahoo.com.au>
2024-11-07 16:35:45 +1030

    fixed collection issue and moved js functions to separate scripts

--- static/submitText.js
+++ static/submitText.js
@@ -1,19 +1,18 @@
-function submitText(fileType, index, text, deckName) {
-    fetch('/update_file', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ type: fileType, index: index, text: text, deckName: deckName })
-    })
-    .then(response => response.json())
-    .then(data => {
-        alert(data.message);
-        window.close(); // Close the popup window
-        window.opener.location.reload(); // Refresh the parent window
-    })
-    .catch(error => {
-        console.error('Error:', error);
-        window.close(); // Ensure the popup closes even if there's an error
-    });
-}
+	function submitText(fileType, index, text, deckName) {
+		fetch('/update_file', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ type: fileType, index: index, text: text, deckName: deckName })
+		})
+		.then(response => response.json())
+		.then(data => {
+			alert(data.message);
+		})
+		.catch(error => {
+			console.error('Error:', error);
+		});
+	}
+
+
